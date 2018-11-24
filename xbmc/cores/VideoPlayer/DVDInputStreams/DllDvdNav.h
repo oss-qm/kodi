d053ced0685d40443391c4469ce01a7f82be2eb5
Enrico Weigelt, metux IT consult <info@metux.net>
2018-11-24 17:48:54 +0000

    HACK: DVD: disable state saving
    Currently needs deeply-internal libdvdnav structures, which we can't access.

--- xbmc/cores/VideoPlayer/DVDInputStreams/DllDvdNav.h
+++ xbmc/cores/VideoPlayer/DVDInputStreams/DllDvdNav.h
@@ -105,8 +105,6 @@ public:
   virtual dvdnav_status_t dvdnav_get_spu_attr(dvdnav_t * self, int32_t streamid, subp_attr_t* stitle_attributes)=0;
   virtual dvdnav_status_t dvdnav_time_search(dvdnav_t * self, uint64_t timepos)=0;
   virtual int64_t dvdnav_convert_time(dvd_time_t *time)=0;
-  virtual dvdnav_status_t dvdnav_get_state(dvdnav_t *self, dvdnav_state_t *save_state)=0;
-  virtual dvdnav_status_t dvdnav_set_state(dvdnav_t *self, dvdnav_state_t *save_state)=0;
   virtual dvdnav_status_t dvdnav_get_angle_info(dvdnav_t *self, int32_t *current_angle,int32_t *number_of_angles)=0;
   virtual dvdnav_status_t dvdnav_angle_change(dvdnav_t *self, int32_t angle) = 0;
   virtual dvdnav_status_t dvdnav_mouse_activate(dvdnav_t *self, pci_t *pci, int32_t x, int32_t y)=0;
@@ -238,10 +236,6 @@ public:
         { return ::dvdnav_time_search(self, timepos); }
     virtual int64_t dvdnav_convert_time(dvd_time_t *time)
         { return ::dvdnav_convert_time(time); }
-    virtual dvdnav_status_t dvdnav_get_state(dvdnav_t *self, dvdnav_state_t *save_state)
-        { return ::dvdnav_get_state(self, save_state); }
-    virtual dvdnav_status_t dvdnav_set_state(dvdnav_t *self, dvdnav_state_t *save_state)
-        { return ::dvdnav_set_state(self, save_state); }
     virtual dvdnav_status_t dvdnav_get_angle_info(dvdnav_t *self, int32_t *current_angle,int32_t *number_of_angles)
         { return ::dvdnav_get_angle_info(self, current_angle, number_of_angles); }
     virtual dvdnav_status_t dvdnav_mouse_activate(dvdnav_t *self, pci_t *pci, int32_t x, int32_t y)
@@ -326,8 +320,6 @@ class DllDvdNav : public DllDynamic, DllDvdNavInterface
   DEFINE_METHOD2(dvdnav_status_t, dvdnav_time_search, (dvdnav_t * p1, uint64_t p2))
   DEFINE_METHOD3(dvdnav_status_t, dvdnav_jump_to_sector_by_time, (dvdnav_t * p1, uint64_t p2, int32_t p3))
   DEFINE_METHOD1(int64_t, dvdnav_convert_time, (dvd_time_t *p1))
-  DEFINE_METHOD2(dvdnav_status_t, dvdnav_get_state, (dvdnav_t *p1, dvdnav_state_t *p2))
-  DEFINE_METHOD2(dvdnav_status_t, dvdnav_set_state, (dvdnav_t *p1, dvdnav_state_t *p2))
   DEFINE_METHOD3(dvdnav_status_t, dvdnav_get_angle_info, (dvdnav_t *p1, int32_t *p2,int32_t *p3))
   DEFINE_METHOD2(dvdnav_status_t, dvdnav_angle_change, (dvdnav_t *p1, int32_t p2))
   DEFINE_METHOD4(dvdnav_status_t, dvdnav_mouse_activate, (dvdnav_t *p1, pci_t *p2, int32_t p3, int32_t p4))
@@ -396,8 +388,6 @@ class DllDvdNav : public DllDynamic, DllDvdNavInterface
     RESOLVE_METHOD(dvdnav_time_search)
     RESOLVE_METHOD(dvdnav_jump_to_sector_by_time)
     RESOLVE_METHOD(dvdnav_convert_time)
-    RESOLVE_METHOD(dvdnav_get_state)
-    RESOLVE_METHOD(dvdnav_set_state)
     RESOLVE_METHOD(dvdnav_get_angle_info)
     RESOLVE_METHOD(dvdnav_angle_change)
     RESOLVE_METHOD(dvdnav_mouse_activate)
